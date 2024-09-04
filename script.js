6389db05e2da4cd68ef9a2b96396132bfb6754c0
FelipeVieira9 <felipe.vieiraa52@gmail.com>
2024-09-04 11:17:47 -0300

    Skins added

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let column = 0;
 let row = 0;
 const frameHeightWidth = 20;
 let frameCount = 0;
-const sprites = {
-    especial: { // To make moviment on scenario grass
+const sprites = { // BUG MAKING CLOUDS SPAWN LATE
+    especial: { // To make moviment on scenario grass, cloud
         scenario_grass: [0, 160, 320, 480, 640, 800, 960, 1120],
         scenario_cloud: [
             {x: 100, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
@@ -20,11 +20,25 @@ const sprites = {
             {x: 700, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
             {x: 770, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
             {x: 800, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
-            {x: 900, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)},  
+            {x: 900, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)},
+            {x: 1000, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1100, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1200, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1300, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1400, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1500, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1600, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)},   
+            {x: 1620, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}, 
+            {x: 1630, y: Math.floor(Math.random() * ((h*0.5) - 20) + 20)}
+
         ]
     },
     birds: {
-        normal: './sprites/bird_sprites/Bird.png'
+        normal: './sprites/bird_sprites/Bird.png',
+        blue: './sprites/bird_sprites/Bird_blue.png',
+        yellow: './sprites/bird_sprites/Bird_yellow.png',
+        green: './sprites/bird_sprites/Bird_green.png',
+        black: './sprites/bird_sprites/Bird_black.png'
     }, 
     obstacles: {
         normal: './sprites/obstacles/wood_pixel.png'
@@ -34,6 +48,18 @@ const sprites = {
         cloud: './sprites/scenario/cloud.png'
     }
 }
+const storageSprites = {
+    actualSprite: sprites.birds.normal,
+    allSprites: [] // -> Make a loop to put all bird sprites in property allSprites 
+}
+
+for (let key in sprites.birds) {
+    storageSprites.allSprites.push(sprites.birds[key]);
+}
+
+let playAnimationShop = false;
+
+console.log(storageSprites);
 // queria usar somente 1 img e mudar somente o src pra cada caso, mas tá acontecendo um bug só aceita somente 1 src pra cada image
 // eu acredito que, por conta de alguma particularidade da classe image que desconheço, não tá dando tempo de carregar a imagem e acaba sobreescrevendo com o ultimo src atribuído no frame
 let img = new Image();
@@ -46,10 +72,13 @@ const restartGame = document.getElementById('restartGame');
 const startGame = document.getElementById('startGame');
 const menu = document.getElementById('menu');
 const menu_title = document.getElementById('menu_title');
+const goShop = document.getElementById('goShop');
+const shop = document.getElementById('shop');
+const returnMenu_button = document.getElementById('returnMenu');
 // 
 
-const canvas = document.querySelector('canvas');
-const ctx = document.querySelector('canvas').getContext('2d');
+const canvas = document.querySelector('#gameWindow');
+const ctx = canvas.getContext('2d');
 
 canvas.setAttribute('width', `${w}px`);
 canvas.setAttribute('height', `${h}px`);
@@ -152,7 +181,7 @@ const givePoints = () => {
 
 // -> Draw Bird
 const drawBird = () => {
-    img.src = sprites.birds.normal;
+    img.src = storageSprites.actualSprite;
     if (frameCount === 8) {
         frameCount = 0;
         if (column === 1) {
@@ -209,6 +238,124 @@ ctx.imageSmoothingQuality = 'high'
         sprites.especial.scenario_cloud.push(newCloud);
       }
 }
+
+//-> Shop
+let t = 0;
+const openShop = (bool) => {
+    if (bool === true) {
+        playAnimationShop = true;
+        menu.style.display = 'none';
+        shop.style.display = 'flex';
+        animateShop();
+    } else {
+        playAnimationShop = false;
+        menu.style.display = 'flex';
+        menu_title.textContent = 'Flappy Bird';
+        restartGame.textContent = 'Start';
+        shop.style.display = 'none';
+    }
+}
+
+const animateShop = () => {
+    if (playAnimationShop === false) {
+        return;
+    }
+
+    // Actual Sprite
+    const canvas_actual = document.querySelector('#actualSprite canvas');
+    const ctx = canvas_actual.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas_actual.width, canvas_actual.height);
+    img.src = storageSprites.actualSprite;
+
+    ctx.drawImage(img, column*frameHeightWidth, row*frameHeightWidth, frameHeightWidth, frameHeightWidth, (canvas_actual.width/2) - frameHeightWidth*2, (canvas_actual.height/2) - frameHeightWidth*2, frameHeightWidth*4, frameHeightWidth*4);
+
+    // All Sprites list
+    storageSprites.allSprites.forEach((src) => {
+        let img = new Image();
+        img.onload = function() {
+            const regex = /(.\/sprites\/bird_sprites\/)|(.png$)/g
+
+            const canvas = document.getElementById(src.replace(regex, ''));
+            const ctx = canvas.getContext('2d');
+            
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, column*frameHeightWidth, row*frameHeightWidth, frameHeightWidth, frameHeightWidth, (canvas.width/2) - frameHeightWidth*2, (canvas.height/2) - frameHeightWidth*2, frameHeightWidth*4, frameHeightWidth*4);
+        }
+        img.src = src;
+
+    })
+    // Next frame
+    if (frameCount === 8) {
+        frameCount = 0;
+        if (column === 1) {
+            if (row === 1) {
+                row = 0;
+                column = 1;
+            } else {
+                row++;
+            }
+        } else {
+            column++;
+        }
+    } else {
+        frameCount++;
+    }
+
+    window.requestAnimationFrame(animateShop);
+}
+
+const updateSkin = (skinId) => {
+    const regex = /(_choose$)/
+    
+
+    // Update ActualSprite
+    const spriteNode = document.getElementById(skinId.target.id).parentNode;
+    storageSprites.actualSprite = `./sprites/bird_sprites/${skinId.target.id.replace(regex, '')}.png`;
+    if (spriteNode.classList[0] === 'noChoosed') {
+        const mainNodeContainer = document.getElementById('spritesChoose_container');
+
+        Array.from(mainNodeContainer.children).forEach((node) => {
+            if(node.classList[0] === 'choosed') {
+                node.classList.remove('choosed');
+                node.classList.add('noChoosed');
+            }
+            console.log(node.classList)
+        })
+
+        spriteNode.classList.toggle('noChoosed');
+        spriteNode.classList.toggle('choosed');
+    } else {
+        
+    }
+}
+
+storageSprites.allSprites.forEach((src, i) => {
+    const regex = /(.\/sprites\/bird_sprites\/)|(.png$)/g
+    let HTML;
+    if (i === 0) {
+        HTML = //noChoosed -> choosed
+        `
+        <div class="choosed">
+            <canvas width="100" height="100" id="${src.replace(regex, '')}"></canvas>
+            <button id="${src.replace(regex, '')+"_choose"}">Choose</button>
+        </div>
+        `
+    } else {
+        HTML = //noChoosed -> choosed
+        `
+        <div class="noChoosed">
+            <canvas width="100" height="100" id="${src.replace(regex, '')}"></canvas>
+            <button id="${src.replace(regex, '')+"_choose"}">Choose</button>
+        </div>
+        `
+    }
+    
+
+    document.getElementById('spritesChoose_container').insertAdjacentHTML('beforeend', HTML);
+    document.getElementById(src.replace(regex, '')+"_choose").addEventListener('click', updateSkin);
+    document.getElementById(src.replace(regex, '')).addEventListener('click', updateSkin);
+})
 // Main function to draw player in canvas, check some property and call functions like createObstacles()
 const draw = () => {
     // Clear Rect
@@ -252,9 +399,10 @@ const draw = () => {
     if (hitPlatform) {
         console.log('Boneco morreu');
         menu.style.display = 'flex';
-        menu_title.textContent = 'Morreu patrão';
+        menu_title.textContent = 'You Died';
         startGame.style.display = 'none';
-        restartGame.style.display = 'block'
+        restartGame.style.display = 'inline-block'
+        restartGame.textContent = 'Restart'
         
         document.querySelector('body').removeEventListener('keydown', enableControls);
         document.querySelector('body').removeEventListener('keyup', enableControls);
@@ -307,3 +455,5 @@ const gameStart = () => {
 
 startGame.addEventListener('click', gameStart);
 restartGame.addEventListener('click', gameStart);
+goShop.addEventListener('click', () => openShop(true));
+returnMenu_button.addEventListener('click', () => openShop(false));
